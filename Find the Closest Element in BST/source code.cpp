a55e785262192e97009837203227e29eedc267b5
RitamPaul <ritampaulblogpost@gmail.com>
2023-06-06 16:01:57 +0530

    updated

--- Find the Closest Element in BST/source code.cpp
+++ Find the Closest Element in BST/source code.cpp
@@ -1,26 +1,24 @@
 class Solution
 {
     public:
-    void func(Node *root, int k, int &ans){
-        if(root==NULL)
-            return;
-        
-        //left traversal
-        func(root->left, k, ans);
-        
-        //calculations
-        int dif = abs(root->data - k);
-        ans = min(ans, dif);
-        
-        //right traversal
-        func(root->right, k, ans);
-    }
-    
     //Function to find the least absolute difference between any node
 	//value of the BST and the given integer.
-    int minDiff(Node *root, int k){
-        int ans=INT_MAX;
-        func(root, k, ans);
-        return ans;
+    int minDiff(Node *root, int k)
+    {
+        Node *ptr=root;
+        int ud=INT_MAX, ld=INT_MAX;
+        while(ptr){
+            if(ptr->data < k){
+                ld = min(ld, abs(ptr->data - k));
+                ptr = ptr->right;
+            }
+            else if(ptr->data > k){
+                ud = min(ud, abs(ptr->data - k));
+                ptr = ptr->left;
+            }
+            else if(ptr->data == k)
+                return 0;
+        }
+        return min(ld, ud);
     }
 };
