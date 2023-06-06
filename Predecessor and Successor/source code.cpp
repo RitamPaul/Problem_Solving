0cdef6e82aa4a6674073b1294c0b81be6b285d76
RitamPaul <ritampaulblogpost@gmail.com>
2023-06-06 17:26:56 +0530

    updated

--- Predecessor and Successor/source code.cpp
+++ Predecessor and Successor/source code.cpp
@@ -1,28 +1,37 @@
 class Solution
 {
-    public:
-    void dfs(Node *root, Node *&pre, Node *&suc, int key){
-        if(root==NULL)
-            return;
-        
-        dfs(root->left, pre, suc, key);
-        
-        if(root->key < key)
-            pre = root;
-        if(suc==NULL && root->key > key){
-            suc = root;
-            return;
-        }
-        
-        dfs(root->right, pre, suc, key);
-        return;
-    }
-    
-    
+    public:   
     void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
     {
         pre=NULL, suc=NULL;
-        dfs(root, pre, suc, key);
+        Node *ptr=root;
+        
+        while(ptr){
+            if(ptr->key < key){
+                pre = ptr;
+                ptr = ptr->right;
+            }
+            else if(ptr->key > key){
+                suc = ptr;
+                ptr = ptr->left;
+            }
+            else if(ptr->key == key){
+                Node * p = ptr->left;
+                Node * q = ptr->right;
+                while(p){
+                    pre = p;
+                    p = p->right;
+                }
+                
+                while(q!=NULL && q->key > key){
+                    suc = q;
+                    q = q->left;
+                }
+                
+                return;
+            }
+        }
+        
         return;
     }
 };
