75b7156d99580bea66e560dacd262894c1b8de41
RitamPaul <ritampaulblogpost@gmail.com>
2023-08-27 10:07:14 +0530

    added

---
+++ Reverse a String/source_code.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    string reverseWord(string &str)
+    {
+        int low=0, high=str.size()-1;
+        while(low <= high){
+            char temp = str[low];
+            str[low] = str[high];
+            str[high] = temp;
+            ++low;
+            --high;
+        }
+        return str;
+    }
+};
