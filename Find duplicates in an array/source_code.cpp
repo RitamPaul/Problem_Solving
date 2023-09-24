df5c0ec6398e8abe1dff43dd4500f72a3cfd854f
RitamPaul <ritampaulblogpost@gmail.com>
2023-09-24 12:11:28 +0530

    added

---
+++ Find duplicates in an array/source_code.cpp
@@ -0,0 +1,16 @@
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        // code here
+        vector<int> ans;
+        for(int i=0; i<n; ++i)
+            arr[ (arr[i] % n) ] += n;
+        for(int i = 0; i < n; ++i){
+            if(arr[i] / n > 1)
+                ans.push_back(i);
+        }
+        if(ans.empty())
+            return {-1};
+        return ans;
+    }
+};
