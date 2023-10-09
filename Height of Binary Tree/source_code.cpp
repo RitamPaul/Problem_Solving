cf0cf9c257934e8732cc94e99d8e9513eed0626f
RitamPaul <ritampaulblogpost@gmail.com>
2023-10-09 22:08:10 +0530

    added
    Co-Authored-By: Dark-Base <115882024+Dark-Base@users.noreply.github.com>

---
+++ Height of Binary Tree/source_code.cpp
@@ -0,0 +1,26 @@
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        // code here
+        // base case
+        if(!node)
+            return 0;
+        return 1+max(height(node->left), height(node->right));
+    }
+};
