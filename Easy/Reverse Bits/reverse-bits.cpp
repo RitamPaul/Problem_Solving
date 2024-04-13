cf98ce21e96f5b3ba08042ad1b8e7fbea1cf9b5a
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2024-04-13 13:29:56 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reverse Bits/reverse-bits.cpp
+++ Easy/Reverse Bits/reverse-bits.cpp
@@ -7,21 +7,11 @@ class Solution {
   public:
     long long reversedBits(long long x) {
         // code here
-        string s = "";
-        for(int i=0; i<32; ++i){
-            if(x & (1<< i))
-                s += '1';
-            else
-                s += '0';
-        }
-        reverse(s.begin(), s.end());
-        
         unsigned long long ans = 0;
-        unsigned long long mask = 1;
         for(int i=0; i<32; ++i){
-            if(s[i] == '1')
-                ans = ans + mask;
-            mask = mask * 2;
+            ans = ans << 1;
+            ans = ans | (x & 1);
+            x = x >> 1;
         }
         return ans;
     }
