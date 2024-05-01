df24cb08a1ba476cb95c421a9e2c60d6f5a994b6
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2024-05-01 20:51:11 +0530

    Time: 3 ms (42.21%) | Memory: 7.5 MB (65.62%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int n=word.size(), ind=-1;
+        for(int i=0; i<n; ++i){
+            if(word[i]==ch){
+                ind = i;
+                break;
+            }
+        }
+        if(ind >= 0)
+            reverse(word.begin(), word.begin()+ind+1);
+        return word;
+    }
+};
