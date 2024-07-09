2e76665af3dc381f192b4859d470d76e49452a14
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2024-07-09 11:51:05 +0530

    Time: 140 ms (84.12%) | Memory: 76.4 MB (95.59%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -3,7 +3,7 @@ public:
     double averageWaitingTime(vector<vector<int>>& cust) {
         int n = cust.size();
         double sum = 0, finish = 0;
-        for(auto &v : cust){
+        for(const auto &v : cust){
             if(finish < v[0])       finish = v[0];
             finish += v[1];
             sum += finish - v[0];
