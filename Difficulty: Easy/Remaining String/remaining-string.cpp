fe0a6a97ee2d2dff2e8056adfba60c1e566484f6
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2024-07-16 16:33:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remaining String/remaining-string.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        // Your code goes here
+        int n=s.size(), i=0;
+        for(; i<n; ++i){
+            if(count == 0)
+                break;
+            if(s[i] == ch)
+                --count;
+        }
+        
+        string ans = "";
+        if(count == 0){
+            for(; i<n; ++i)
+                ans += s[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        char ch;
+        int count;
+
+        cin >> s >> ch >> count;
+        Solution ob;
+        cout << ob.printString(s, ch, count) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
