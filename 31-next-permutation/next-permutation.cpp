9376fd899b2e011066e22f6e4901b39b496ca0cb
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2024-07-18 22:05:36 +0530

    Time: 9 ms (14.88%) | Memory: 14.8 MB (5.04%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
+    int search(vector<int> &nums, int start, int end, int target){
+        int ans = -1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(target < nums[mid]){
+                ans = mid;
+                start = mid + 1;
+            }
+            else
+                end = mid - 1;
+        }
+        return ans;
+    }
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n=nums.size(), i, x, y;
+        for(i=n-2; i>=0; --i){
+            if(nums[i] < nums[i+1])         break;
+        }
+        if(i < 0){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        x = i;
+        y = search(nums, x+1, n-1, nums[x]);
+        swap(nums[x], nums[y]);
+        reverse(nums.begin()+x+1, nums.end());
     }
 };
