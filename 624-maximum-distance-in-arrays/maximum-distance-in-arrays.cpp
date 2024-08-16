513c19bc164c5af9cb3793fd8ba99d23a3cf224d
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2024-08-16 14:01:59 +0530

    Time: 271 ms (8.96%) | Memory: 118.8 MB (20.28%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int n = arrays.size();
+        auto n{arrays.size()};
         vector<pair<int,int>> mini, maxi;
 
-        for(int i=0; i<n; ++i){
-            int m = arrays[i].size();
+        for(auto i=0; i<n; ++i){
+            auto m{arrays[i].size()};
             mini.push_back({arrays[i][0], i});
             maxi.push_back({arrays[i][m-1], i});
         }
@@ -13,16 +13,16 @@ public:
         sort(mini.begin(), mini.end());
         sort(maxi.begin(), maxi.end());
 
-        int mini1val=mini[0].first;
-        int mini1pos=mini[0].second;
-        int maxi1val=maxi[n-1].first;
-        int maxi1pos=maxi[n-1].second;
+        auto mini1val{mini[0].first};
+        auto mini1pos{mini[0].second};
+        auto maxi1val{maxi[n-1].first};
+        auto maxi1pos{maxi[n-1].second};
 
         if(mini1pos == maxi1pos){
-            int mini2val = mini[1].first;
-            int maxi2val = maxi[n-2].first;
-            int big1 = maxi2val - mini1val;
-            int big2 = maxi1val - mini2val;
+            auto mini2val{mini[1].first};
+            auto maxi2val{maxi[n-2].first};
+            auto big1{maxi2val - mini1val};
+            auto big2{maxi1val - mini2val};
             return max(big1, big2);
         }
         return maxi1val - mini1val;
