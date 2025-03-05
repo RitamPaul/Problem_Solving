a1891b7121a175bbe30ad8a06e457871a763a0d6
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2025-03-05 13:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (61.42%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long coloredCells(long long n) {
+        // right_angle_triangle - repeated_walls + 1_center_core
         return (n*(n+1)/2)*4 - (n*4) + 1;
     }
 };
