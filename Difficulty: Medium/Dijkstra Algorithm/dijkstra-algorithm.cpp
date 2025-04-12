4911fc0eb0041798c5537bc66d6e49b5b2f8839c
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2025-04-12 19:57:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,85 +1,79 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 // } Driver Code Ends
 
-
-bool vis[1000+1];
-class Solution
-{
-public:
-    vector<int> dijkstra(int v, vector<vector<int>> adj[], int src)
-    {
-        vector<int> dist(v, INT_MAX);
-        memset(vis, false, sizeof vis);
-
-        priority_queue<
-            pair<int, int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>> pq;
-
-        dist[src] = 0;
-        pq.push({ dist[src], src });
-
-        while (pq.size()) {
-            int curnode = pq.top().second;
-            int curdist = pq.top().first;
+// User Function Template
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto &e : edges){
+            int u=e[0];
+            int v=e[1];
+            int w=e[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        vector<int> shortest(V,INT_MAX);
+        shortest[src]=0;
+        // default max heap but will use as min heap
+        priority_queue<pair<int,int>> pq;
+        pq.push({0,src});
+        while(pq.size()){
+            auto p=pq.top();
             pq.pop();
-            vis[curnode] = true;
-
-            for (const auto &neigh : adj[curnode]) {
-                int neinode = neigh[0];
-                int neidist = neigh[1];
-
-                if (curdist + neidist < dist[neinode]) {
-                    dist[neinode] =  curdist + neidist;
-                    pq.push({ dist[neinode], neinode });
+            int curweight=-1*p.first;
+            int curnode=p.second;
+            for(auto &neighpair : adj[curnode]){
+                int neighnode=neighpair.first;
+                int neighweight=neighpair.second;
+                if(curweight+neighweight < shortest[neighnode]){
+                    shortest[neighnode]=curweight+neighweight;
+                    pq.push({-1*(curweight+neighweight),neighnode});
                 }
             }
         }
-
-        return dist;
+        return shortest;
     }
 };
 
 
+
 //{ Driver Code Starts.
 
-
-int main()
-{
+int main() {
     int t;
     cin >> t;
     while (t--) {
         int V, E;
         cin >> V >> E;
-        vector<vector<int>> adj[V];
-        int i=0;
-        while (i++<E) {
+        vector<vector<int>> edges;
+        int i = 0;
+        while (i++ < E) {
             int u, v, w;
             cin >> u >> v >> w;
-            vector<int> t1,t2;
-            t1.push_back(v);
-            t1.push_back(w);
-            adj[u].push_back(t1);
-            t2.push_back(u);
-            t2.push_back(w);
-            adj[v].push_back(t2);
+            edges.push_back({u, v, w});
+            edges.push_back({v, u, w});
         }
-        int S;
-        cin>>S;
-        
+        int src;
+        cin >> src;
+        cin.ignore();
+
         Solution obj;
-    	vector<int> res = obj.dijkstra(V, adj, S);
-    	
-    	for(int i=0; i<V; i++)
-    	    cout<<res[i]<<" ";
-    	cout<<endl;
+        vector<int> res = obj.dijkstra(V, edges, src);
+
+        for (int i = 0; i < V; i++)
+            cout << res[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
     }
 
     return 0;
 }
-
-
 // } Driver Code Ends
