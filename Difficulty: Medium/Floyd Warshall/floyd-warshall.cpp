dc95fdaf7a57393e6adcbd3b09b820b684e163f9
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2025-04-16 14:20:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        // Code here
+        int n=dist.size();
+        for(int via=0; via<n; ++via){
+            for(int i=0; i<n; ++i){
+                for(int j=0; j<n; ++j){
+                    if(dist[i][via]!=(int)1e8 and dist[via][j]!=(int)1e8)
+                        dist[i][j]=min(dist[i][j], dist[i][via]+dist[via][j]);
+                }
+            }
+        }
+        return;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution obj;
+        obj.floydWarshall(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
