072fbe0a8ebab973dd5a8d72e086f7082aba1372
Ritam Paul <114846001+RitamPaul@users.noreply.github.com>
2025-07-06 00:51:49 +0530

    Time: 1310 ms (77.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+select sp.name
+from
+(SalesPerson sp) left join (Orders o join Company c on o.com_id=c.com_id)
+on
+sp.sales_id=o.sales_id
+where c.name is null or sp.sales_id not in (
+    select sp1.sales_id 
+    from
+    (SalesPerson sp1) join (Orders o join Company c on o.com_id=c.com_id)
+    on
+    sp1.sales_id=o.sales_id
+    where sp1.name=sp.name and c.name="RED"
+)
+group by sp.name;
